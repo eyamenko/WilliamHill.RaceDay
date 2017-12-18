20254cf9b5c6975d111bd0f0e9be0308908e8844
Eugene Yamenko <yamenko@me.com>
2017-12-18 20:31:01 +1100

    Extract an interface from the Race Service

---
+++ src/WilliamHill.RaceDay.Contracts/Services/IRaceService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WilliamHill.RaceDay.Models.ViewModels;
+
+namespace WilliamHill.RaceDay.Contracts.Services
+{
+    public interface IRaceService
+    {
+        Task<IList<Race>> GetRaces();
+    }
+}
--- src/WilliamHill.RaceDay.Services/RaceService.cs
+++ src/WilliamHill.RaceDay.Services/RaceService.cs
@@ -1,7 +1,24 @@
-﻿namespace WilliamHill.RaceDay.Services
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WilliamHill.RaceDay.Contracts.ApiClients;
+using WilliamHill.RaceDay.Contracts.Services;
+using WilliamHill.RaceDay.Models.ViewModels;
+
+namespace WilliamHill.RaceDay.Services
 {
-    public class RaceService
+    public class RaceService : IRaceService
     {
-        
+        private readonly IWhatechApiClient _apiClient;
+
+        public RaceService(IWhatechApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task<IList<Race>> GetRaces()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
