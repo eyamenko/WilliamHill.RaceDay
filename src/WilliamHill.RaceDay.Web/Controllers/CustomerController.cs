b397edcaadbdf4b1ac979242f2b523dea7f34653
Eugene Yamenko <yamenko@me.com>
2017-12-18 19:57:39 +1100

    Add Race and Customer controllers

---
+++ src/WilliamHill.RaceDay.Web/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WilliamHill.RaceDay.Web.Controllers
+{
+    [Route("api/customers")]
+    public class CustomerController : Controller
+    {
+        [HttpGet("")]
+        public async Task<IActionResult> GetCustomers()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
---
+++ src/WilliamHill.RaceDay.Web/Controllers/RaceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WilliamHill.RaceDay.Web.Controllers
+{
+    [Route("api/races")]
+    public class RaceController : Controller
+    {
+        [HttpGet("")]
+        public async Task<IActionResult> GetRaces()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
